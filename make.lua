000edff3f26e3dd018a89719f7f86351f46d1b6d
cxxsxr <64624590+cxxsxr@users.noreply.github.com>
2026-07-05 20:40:11 +0200

    dynamic Mac handling

--- make.lua
+++ make.lua
@@ -1,7 +1,14 @@
 require( "ggbuild.gen_ninja" )
 require( "ggbuild.git_version" )
 
+-- Detect architecture and set macOS ARM64 flag
+local target_arch = io.popen("uname -m"):read("*l")
+local is_macos_arm64 = (OS == "macos" and target_arch == "arm64")
+
 global_cxxflags( "-I source -I libs" )
+if is_macos_arm64 then
+	global_cxxflags( "-I/opt/homebrew/include" ) -- Homebrew headers for ARM64
+end
 
 msvc_global_cxxflags( "/std:c++20 /W4 /wd4100 /wd4146 /wd4189 /wd4201 /wd4307 /wd4324 /wd4351 /wd4127 /wd4505 /wd4530 /wd4702 /wd4706 /D_CRT_SECURE_NO_WARNINGS" )
 msvc_global_cxxflags( "/wd4244 /wd4267" ) -- silence conversion warnings because there are tons of them
@@ -17,8 +24,14 @@ gcc_global_cxxflags( "-Wno-unused-parameter -Wno-missing-field-initializers" )
 gcc_global_cxxflags( "-Wno-switch" ) -- this is too annoying in practice
 gcc_global_cxxflags( "-D_LIBCPP_REMOVE_TRANSITIVE_INCLUDES" )
 
-if OS == "linux" then
+-- Limit SSE to x86_64 Linux only (skip ARM)
+if OS == "linux" and target_arch == "x86_64" then
 	gcc_global_cxxflags( "-msse4.2 -mpopcnt" )
+end
+
+-- Native Apple Silicon flags and minimum deployment target
+if is_macos_arm64 then
+	gcc_global_cxxflags( "-arch arm64 -mmacosx-version-min=11.0" )
 end
 
 if config == "release" then
@@ -70,6 +83,41 @@ obj_cxxflags( "source/client/renderer/backend.cpp", "-I libs/sdl" )
 obj_cxxflags( "source/qcommon/linear_algebra_kernels.cpp", "-O2" )
 
 do
+	-- Build client libs list, skipping static SDL/OpenAL on macOS ARM64
+	local client_libs = {
+		"imgui",
+		"cgltf",
+		"clay",
+		"discord",
+		"dr_mp3",
+		"freetype",
+		"ggentropy",
+		"ggformat",
+		"ggtime",
+		"glad",
+		"jsmn",
+		"luau",
+		"monocypher",
+		"picohttpparser",
+		"stb_image",
+		"stb_image_write",
+		"stb_rect_pack",
+		"stb_vorbis",
+		"tracy",
+		"zstd",
+		platform_curl_libs,
+	}
+	if not is_macos_arm64 then
+		table.insert( client_libs, "sdl" )
+		table.insert( client_libs, "openal" )
+	end
+
+	-- Base macOS linker flags, extended with Homebrew paths and dynamic libs for ARM64
+	local macos_ld = "-lcurl -framework AudioToolbox -framework Cocoa -framework CoreAudio -framework CoreHaptics -framework CoreVideo -framework IOKit -framework GameController -framework ForceFeedback -framework Carbon -framework UniformTypeIdentifiers -framework QuartzCore"
+	if is_macos_arm64 then
+		macos_ld = macos_ld .. " -L/opt/homebrew/lib -lSDL2 -lopenal"
+	end
+
 	bin( "client", {
 		srcs = {
 			"source/cgame/*.cpp",
@@ -80,37 +128,12 @@ do
 			"source/server/sv_*.cpp",
 		},
 
-		libs = {
-			"imgui",
-
-			"cgltf",
-			"clay",
-			"discord",
-			"dr_mp3",
-			"freetype",
-			"ggentropy",
-			"ggformat",
-			"ggtime",
-			"glad",
-			"jsmn",
-			"luau",
-			"monocypher",
-			"openal",
-			"picohttpparser",
-			"sdl",
-			"stb_image",
-			"stb_image_write",
-			"stb_rect_pack",
-			"stb_vorbis",
-			"tracy",
-			"zstd",
-			platform_curl_libs,
-		},
+		libs = client_libs,
 
 		rc = "source/client/platform/client",
 
 		windows_ldflags = "shell32.lib gdi32.lib ole32.lib oleaut32.lib ws2_32.lib crypt32.lib winmm.lib version.lib imm32.lib advapi32.lib setupapi.lib /SUBSYSTEM:WINDOWS",
-		macos_ldflags = "-lcurl -framework AudioToolbox -framework Cocoa -framework CoreAudio -framework CoreHaptics -framework CoreVideo -framework IOKit -framework GameController -framework ForceFeedback -framework Carbon -framework UniformTypeIdentifiers -framework QuartzCore",
+		macos_ldflags = macos_ld,
 		linux_ldflags = "-lm -lpthread -ldl",
 		no_static_link = true,
 	} )
